a9e940c772ca242aec51d296111d167296d09256
“Avishank” <avishank.21b1541084@abes.ac.in>
2024-01-03 12:44:30 +0530

    project added

---
+++ MERN-ecommerce-Frontend/src/pages/StripeCheckout.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useSelector } from 'react-redux';
+import CheckoutForm from "./CheckoutForm";
+import "../Stripe.css";
+import { selectCurrentOrder } from "../features/order/orderSlice";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render. 
+// This is your test publishable API key.
+const stripePromise = loadStripe("pk_test_51NwmFQSFkXZpFFtEZDmXTB8Y4BYkB6QBmz5PV9fm7fVvqW471NVzEnb2aensdRMLOuPqqZeA5M6XJVKY233pRhdt00PzyJ48yD");
+
+export default function StripeCheckout() {
+  const [clientSecret, setClientSecret] = useState("");
+  const currentOrder = useSelector(selectCurrentOrder)
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ totalAmount: currentOrder.totalAmount, orderId:currentOrder.id }),
+    
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="Stripe">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
